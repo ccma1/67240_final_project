1b3e58284f4aae2f1bda6b764659b71f242f1b59
Cheng Ma <md11chm@gmail.com>
2021-12-02 01:05:22 -0500

    final handin

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function validateForm() {
-    console.log("hi");
+    //gets all the forms fields
     var fields = document.getElementsByClassName("item");
     var correct_form = true;
     //iterate through all form fields
     for (let i = 0; i < fields.length; i++) {
+        //gets field, gets error, and input into field
         var field_item = fields[i];
         var field_item_error = field_item.getElementsByClassName("error")[0];
         var field_item_input;
@@ -12,6 +13,7 @@ function validateForm() {
         } else {
             field_item_input = field_item.getElementsByTagName("input")[0].value;
         }
+        //checks that field is non empty
         if (field_item_input.length == 0) {
             field_item_error.textContent = "Field cannot be empty";
             field_item_error.className = "error active";
@@ -20,6 +22,7 @@ function validateForm() {
             field_item_error.textContent = "";
             field_item_error.className = "error";
         }
+        //checks that email is valid
         if (field_item.className.includes("email")) {
             if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field_item_input))) {
                 field_item_error.textContent = "Invalid email entered";
@@ -32,6 +35,7 @@ function validateForm() {
         }
 
     }
+    //resets form if all fields are valid
     if (correct_form) {
         document.getElementsByTagName("form")[0].reset();
     }
